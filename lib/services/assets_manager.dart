8f375d5c17891f4bddd0ba12f9c9c5ac4e33ae51
thesheerazali <sheerazali7676@gmail.com>
2024-01-21 15:08:59 +0500

    Services

---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,7 @@
+class AssetsManager {
+  static String imagePath = "assets/images";
+  static String userImage = "$imagePath/user_icon.png";
+  static String botImage = "$imagePath/chat_logo.png";
+  static String openaiLogo = "$imagePath/dexfin_logo.png";
+  static String dexfixBetaLogo = "$imagePath/dexfixbet.png";
+}
